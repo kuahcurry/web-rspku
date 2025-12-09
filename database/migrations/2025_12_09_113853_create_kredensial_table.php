9e0db3020598a523928d39a3ed35594508eb1f07
kuahcurry <aqefhakimi32@gmail.com>
2025-12-09 22:16:51 +0700

    update necessary functions

--- database/migrations/2025_12_09_113853_create_kredensial_table.php
+++ database/migrations/2025_12_09_113853_create_kredensial_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('kredensial', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('user_id')->constrained('users_registration')->onDelete('cascade');
             $table->date('tanggal_berlaku');
             $table->date('tanggal_selesai')->nullable();
             $table->string('nama_kegiatan');
